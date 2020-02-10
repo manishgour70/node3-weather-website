8dc4fc3eca78da308168a561534c9b68786c0b6a
Manish <manishgour70.com>
2020-02-10 17:33:54 +0530

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const express=require('express')
 const hbs=require('hbs')
 
 const app=express()
+const port=process.env.PORT || 3000
 
 //Define paths for express config
 const publicDirectoryPath=path.join(__dirname,'../public')
@@ -98,6 +99,6 @@ app.get('*',(req,res)=>{
 // app.get('/weather',(req,res)=>{
 //     res.send('<h1>weather is cool</h1>')
 // })
-app.listen(3000,()=>{
-    console.log('server is running at port 3000')
+app.listen(port,()=>{
+    console.log('server is running at port '+port)
 })
